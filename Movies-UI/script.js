e5db72e6f2780d2d2f85e79b38df43a2fa139d5c
chicodevbr <chicodevbr@gmail.com>
2022-01-02 22:33:36 -0300

    update key

--- Movies-UI/script.js
+++ Movies-UI/script.js
@@ -1,4 +1,4 @@
-const KEY = process.env.KEY;
+const key = process.env.KEY;
 
 const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${key}&page=1`;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query="`;
